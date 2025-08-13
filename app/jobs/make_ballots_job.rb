e02262decbefbb1d70d8a72410878f188eb26eb5
blackhat-hemsworth <114207848+blackhat-hemsworth@users.noreply.github.com>
2025-08-13 17:08:28 -0500

    ballot view update, editing added

--- app/jobs/make_ballots_job.rb
+++
@@ -1,9 +0,0 @@
-class MakeBallotsJob < ApplicationJob
-  queue_as :urgent
-
-  def perform(ballots, participants)
-   participants.each do |p|
-      @ballot.make_submission(p)
-   end
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :elections do
     resources :participants, only: %i[destroy]
-    resources :ballots, only: %i[index new show create destroy] do
+    resources :ballots do
       member do
         post :send_ballots
       end
